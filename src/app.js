17cd3d3bf11d4351c4aec100dc7ffd5e58ad72e9
agusalvs <agustina.alvarezsacra@gmail.com>
2023-01-04 20:47:02 +0000

    Bubble Sorting Cards

--- src/app.js
+++ src/app.js
@@ -5,7 +5,57 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function randomCard() {
+  const palos = ["♦", "♥", "♠", "♣"];
+
+  let indexPalo = Math.floor(Math.random() * palos.length);
+
+  let top = document.querySelector(".top");
+  top.innerHTML = palos[indexPalo];
+
+  let bottom = document.querySelector(".bottom");
+  bottom.innerHTML = palos[indexPalo];
+
+  if (palos[indexPalo] == "♥" || palos[indexPalo] == "♦") {
+    top.style.color = "red";
+  } else {
+    top.style.color = "black";
+  }
+  if (palos[indexPalo] == "♥" || palos[indexPalo] == "♦") {
+    bottom.style.color = "red";
+  } else {
+    bottom.style.color = "black";
+  }
+
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNum = Math.floor(Math.random() * numbers.length);
+  let number = document.querySelector(".number");
+  number.innerHTML = numbers[indexNum];
+  // let bot = document.querySelector(".bot");
+  // bot = top;
+  for (let i = 1; i <= 3; i++) {
+    document.body.innerHTML += `<div class="card m-auto" style="width: 18rem; height: 26rem;">
+    <div class="card-body">
+      <div class="top">${palos[indexPalo]}</div>
+
+      <div class="number">${numbers[indexNum]}</div>
+      
+      <div class="bottom">${palos[indexPalo]}</div>
+    </div>
+  </div>`;
+  }
 };
